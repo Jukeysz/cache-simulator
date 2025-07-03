76210b3feffd17ea2d52f62816fcc275d4e8db2c
Jukeysz <scaglioni.ju@gmail.com>
2025-07-02 21:29:24 -0300

    bingus-chungus

--- src/main/java/org/example/cache_simulator.java
+++ src/main/java/org/example/cache_simulator.java
@@ -1,3 +1,7 @@
+package org.example;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 
 public class cache_simulator {
     public static void main(String[] args) {
@@ -10,11 +14,18 @@ public class cache_simulator {
         int nsets = Integer.parseInt(args[0]);
         int bsize = Integer.parseInt(args[1]);
         int assoc = Integer.parseInt(args[2]);
+
+        int []cache_val = new int[nsets * assoc];
+        int []cache_tag = new int[nsets * assoc];
+
+        int n_bits_offset = (int) (Math.log(nsets) / Math.log(2));
+        int n_bits_indice = (int) (Math.log(nsets) / Math.log(2));
+
+        int n_bites_tag = 32 - n_bits_offset - n_bits_indice;;
+
         String subst = args[3];
         int flagOut = Integer.parseInt(args[4]);
         String arquivoEntrada = args[5];
-
-
 
         System.out.printf("nsets = %d\n", nsets);
         System.out.printf("bsize = %d\n", bsize);
@@ -23,9 +34,18 @@ public class cache_simulator {
         System.out.printf("flagOut = %d\n", flagOut);
         System.out.printf("arquivo = %s\n", arquivoEntrada);
 
-        
         // Seu codigo vai aqui
 
+        int hits = 0, misses = 0;
+        int time = 0;
 
+        try (DataInputStream dis = new DataInputStream(new FileInputStream(arquivoEntrada))) {
+            while (dis.available() > 0) {
+                int address = dis.readInt();
+
+                int offset = address & ((1 << n_bits_offset) - 1);
+
+            }
+        } catch ();
     }
 }
